dd0db48875ca39322ed6bb8dbb84015a22101e27
Hannes Rabo <hannes.rabo@gmail.com>
2018-09-13 21:10:15 +0200

    Removed duplicate file

--- lab1/start_hadoop.sh
+++
@@ -1,7 +0,0 @@
-# Start hadoop
-$HADOOP_HOME/sbin/hadoop-daemon.sh start namenode
-$HADOOP_HOME/sbin/hadoop-daemon.sh start datanode
-
-# Start the ssh server
-sudo systemctl start sshd
-$HBASE_HOME/bin/start-hbase.sh
